5ec2a0d6e7b1a6068163f5b25adb7a99b79df2b1
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2018-10-20 20:32:23 -0700

    Update credentials based on Heroku

--- Database/Database.php
+++ Database/Database.php
@@ -3,7 +3,7 @@ class Database{
     private $dbservername = "wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     private $dbusername = "u8fndnr8j6kxeb6c";
     private $dbpassword = "vxb0s8tl2etx2rwe";
-    private $dbname = "php2-store";
+    private $dbname = "yqn7iwawbmw2sk4q";
     
     function getConnection() { 
         $conn = new mysqli($this->dbservername, $this->dbusername, $this->dbpassword, $this->dbname);
